0edf84a6774d73c3d54718bcff5df5813aeebe33
Tashrif Radin Ali <radin00@hotmail.com>
2025-04-13 18:13:51 -0400

    ui change

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          Game: path.resolve(__dirname, 'game.html'),
+          ChapterSolution: path.resolve(__dirname, 'ChapterSolution.html'),
+          Setup: path.resolve(__dirname, 'howtoplay.html'),
+        },
+      },
+    },
+  });
+  
